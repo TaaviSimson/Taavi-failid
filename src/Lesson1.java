ef506e51f5fdb083a4ebb1a46d3e4b5a62993485
TaaviSimson <taavi.simson@gmail.com>
2020-08-24 14:07:27 +0300

    Ülesanne 1 muudatused  II

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -27,7 +27,13 @@ public class Lesson1 {
         int x1 = 10;
         int x2 = 20;
         // Tekita muutuja y1 = ++x1, trüki välja nii x1 kui y1
+        int y1 = ++x1;
+        System.out.println(x1);
+        System.out.println(y1);
         // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
+        int y2 = x2++;
+        System.out.println(x2);
+        System.out.println(y2);
         // Analüüsi tulemusi
     }
 
@@ -35,16 +41,26 @@ public class Lesson1 {
         // TODO
         // Loo arvulised muutujad
         // a = 18 % 3
+        int a = 18 % 3;
         // b = 19 % 3
+        int b = 19 % 3;
         // c = 20 % 3
+        int c = 20 % 3;
         // d = 21 % 3
+        int d = 21 % 3;
         // Prindi välja kõigi muutujate väärtused
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 
     public static void excersie4(){
         // TODO
         // Defineeri String tüüpi muutuja mille sisu oleks „\“\\““
+        String a = "\"\\\"\\\\\"\"";
         // Trüki muutuja sisu välja
+        System.out.println(a);
     }
 
 }
