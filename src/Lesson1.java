8827fce099424dce0c59abdb31c5a382ae06dc90
TaaviSimson <taavi.simson@gmail.com>
2020-08-24 13:07:02 +0300

    Ülesanne 1 muudatused

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -10,14 +10,22 @@ public class Lesson1 {
     public static void excersie1(){
         // TODO
         // defineeri 3 muutujat a = 1, b = 1, c = 3
+        int a = 1;
+        int b = 1;
+        int c = 3;
         // Prindi välja a==b ja a==c
+        System.out.println(a==b && a==c);
         // Lisa rida a = c
+        a = c;
         // Prindi välja a==b ja a==c, mis muutus???
+        System.out.println(a==b && a==c);
     }
 
     public static void excersie2(){
         // TODO
         // Loo muutujad x1 = 10 ja x2 = 20, vali sobiv andmetüüp
+        int x1 = 10;
+        int x2 = 20;
         // Tekita muutuja y1 = ++x1, trüki välja nii x1 kui y1
         // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
         // Analüüsi tulemusi
