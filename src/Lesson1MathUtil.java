5af2b2072686f1ba0153e03d58387a8cf16c4e61
TaaviSimson <taavi.simson@gmail.com>
2020-08-25 10:30:54 +0300

    25.08.2020 update III

--- src/Lesson1MathUtil.java
+++ src/Lesson1MathUtil.java
@@ -5,30 +5,53 @@ public class Lesson1MathUtil {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Sistesta soovitud test: min, max, abs, isEven, min1 või max2");
+        System.out.println("Sistesta soovitud test: min, max, abs, isEven, min2 või max2");
         String test = scanner.nextLine(); //Loeb millist teksti hakkame tegema
-        System.out.println("Sisesta parameetrid a, b ja c");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
         System.out.println("");//jätab rea vahele
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c + "\n");//jätab rea vahele (+"\n")
-        System.out.println("Vastus:");
+
 
         //String test = "min";
         if (test.equalsIgnoreCase("min")){
+            System.out.println("Sisesta parameetrid a ja b");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("");//jätab rea vahele
+            System.out.println("Vastus:");
             System.out.println(min(a,b));
         } else if (test.equalsIgnoreCase("abs")){
+            System.out.println("Sisesta parameetrer a");
+            int a = scanner.nextInt();
+            System.out.println("");//jätab rea vahele
+            System.out.println("Vastus:");
             System.out.println(abs(a));
         } else if (test.equalsIgnoreCase("max")){
+            System.out.println("Sisesta parameetrid a ja b");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("");//jätab rea vahele
+            System.out.println("Vastus:");
             System.out.println(max(a,b));
         } else if (test.equalsIgnoreCase("isEven")){
+            System.out.println("Sisesta parameetrer a");
+            int a = scanner.nextInt();
+            System.out.println("");//jätab rea vahele
+            System.out.println("Vastus:");
             System.out.println(isEven(a));
         } else if (test.equalsIgnoreCase("min2")){
+            System.out.println("Sisesta parameetrid a, b ja c");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            System.out.println("");//jätab rea vahele
+            System.out.println("Vastus:");
             System.out.println(min2(a,b,c));
         } else {
+            System.out.println("Sisesta parameetrid a, b ja c");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            System.out.println("");//jätab rea vahele
+            System.out.println("Vastus:");
             System.out.println(max2(a, b, c));
         }
 
