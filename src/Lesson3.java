b2e3fb01dea948b29eaf1a024fdc1035e0e544af
TaaviSimson <taavi.simson@gmail.com>
2020-08-26 15:52:39 +0300

    26.08.2020 update XVII

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.Random;
+//import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson3 {
--- src/Lesson3Hard.java
+++ src/Lesson3Hard.java
@@ -1,13 +1,25 @@
 import java.util.Random;
+import java.util.Scanner;
 
 // Enne kui seda tegema hakkad tee ära Lesson 2 (välja arvatud ülesanded 6, 8, 9)
 public class Lesson3Hard {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sisestka käsklus: evenFibonacci");
+        String kasklus = scanner.nextLine();
+        System.out.println("");
+        if (kasklus.equalsIgnoreCase("evenFibonacci")){
+            System.out.println("Sisesta number milleni soovid Fibonacci jada paaris arvud liita");
+            int a = scanner.nextInt();
+            System.out.println("\n"+"Arvuni "+a+" on Fibonacci jada paaris numbrite summa on "+evenFibonacci(a));
+        } else{
+            System.out.println("Sisestasid tundmatu käskluse");
+        }
     }
 
     public static int evenFibonacci(int x){
         // TODO liida kokku kõik paaris fibonacci arvud kuni numbrini x
+
         return 0;
     }
 
