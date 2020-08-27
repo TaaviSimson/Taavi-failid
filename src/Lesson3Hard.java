9ec232f53f10690d60d3fb158194d579b7efbba6
TaaviSimson <taavi.simson@gmail.com>
2020-08-27 10:25:02 +0300

    27.08.2020 update XXI

--- src/Lesson3Hard.java
+++ src/Lesson3Hard.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Lesson3Hard {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Sisestka käsklus: evenFibonacci");
+        System.out.println("Sisestka käsklus: evenFibonacci, randomGame");
         String kasklus = scanner.nextLine();
         System.out.println("");
         if (kasklus.equalsIgnoreCase("evenFibonacci")) {
             System.out.println("Sisesta number milleni soovid Fibonacci jada paaris arvud liita");
             int a = scanner.nextInt();
             System.out.println("\n" + "Arvuni " + a + " on Fibonacci jada paaris numbrite summa on " + evenFibonacci(a));
+        } else if (kasklus.equalsIgnoreCase("randomGame")) {
+            randomGame();
         } else {
             System.out.println("Sisestasid tundmatu käskluse");
         }
@@ -40,9 +42,28 @@ public class Lesson3Hard {
         // iga kord pärast kasutaja sisestatud täis arvu peab programm ütlema kas number oli suurem või väiksem
         // ja kasutaja peab saama uuesti arvata
         // numbri ära aramise korral peab programm välja trükkima mitu katset läks numbri ära arvamiseks
+        System.out.println("Arva ära number vahemikus 0-100");
+        System.out.println("Sul on 10 katset");
         Random random = new Random();
-        int i = random.nextInt(100);
-        System.out.println(i);
+        int i = random.nextInt(100); //Voi (int) (Math.random() * 100 +1);
+        Scanner scanner = new Scanner(System.in);
+        int j = 1;
+        int katse = 10; //Katseta arv
+        boolean vastus = false;
+        while (vastus = false || j <= katse) {
+            j++;
+            int a = scanner.nextInt();
+            if (a == i) {
+                System.out.println("\n" + "Arvasid õigesti. Vastus oli "+i);
+                vastus = true;
+            } else if (j > katse) { //See plokk siia, et tarbetuid lisaridasid ei kirjutaks
+                System.out.println("\n"+"Katsed said otsa, vastus oli " + i);
+            } else if (a > i) {
+                System.out.println("\n" + "Otsitav number on väiksem");
+            } else {
+                System.out.println("\n" + "Otsitav number on suurem");
+            }
+        }
     }
 
     public static String morseCode(String text) {
